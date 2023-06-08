74dc9cc46ca31f962cdc0961a252dc8ef65d10d3
Sergey Petrachkov <petrachkovsergey@gmail.com>
2023-06-08 20:07:58 +0200

    add `storedProperties` func

--- Sources/PublicMemeberwiseInitMacroMacros/PublicMemeberwiseInitMacroMacro.swift
+++ Sources/PublicMemeberwiseInitMacroMacros/PublicMemeberwiseInitMacroMacro.swift
@@ -12,11 +12,7 @@ public struct PublicMemberwiseInitMacro: MemberMacro {
         guard let classDeclaration = declaration.as(ClassDeclSyntax.self) else {
             return []
         }
-        let storedProperties = classDeclaration
-            .memberBlock
-            .members
-            .compactMap { $0.decl.as(VariableDeclSyntax.self) }
-            .filter { $0.bindings.first?.accessor == nil }
+        let storedProperties = classDeclaration.storedProperties()
 
         let initArguments = storedProperties.compactMap { property -> (name: String, type: String)? in
             guard let patternBinding = property.bindings.first?.as(PatternBindingSyntax.self) else {
@@ -44,6 +40,55 @@ public struct PublicMemberwiseInitMacro: MemberMacro {
             return []
         }
         return [finalDeclaration]
+    }
+}
+
+extension VariableDeclSyntax {
+    /// Determine whether this variable has the syntax of a stored property.
+    ///
+    /// This syntactic check cannot account for semantic adjustments due to,
+    /// e.g., accessor macros or property wrappers.
+    var isStoredProperty: Bool {
+        if bindings.count != 1 {
+            return false
+        }
+
+        let binding = bindings.first!
+        switch binding.accessor {
+        case .none:
+            return true
+        case .accessors(let node):
+            for accessor in node.accessors {
+                switch accessor.accessorKind.tokenKind {
+                case .keyword(.willSet), .keyword(.didSet):
+                    // Observers can occur on a stored property.
+                    break
+                default:
+                    // Other accessors make it a computed property.
+                    return false
+                }
+            }
+            return true
+        case .getter:
+            return false
+        @unknown default:
+            return false
+        }
+    }
+}
+
+extension DeclGroupSyntax {
+    /// Enumerate the stored properties that syntactically occur in this
+    /// declaration.
+    func storedProperties() -> [VariableDeclSyntax] {
+        return memberBlock.members.compactMap { member in
+            guard let variable = member.decl.as(VariableDeclSyntax.self),
+                  variable.isStoredProperty else {
+                return nil
+            }
+
+            return variable
+        }
     }
 }
 
